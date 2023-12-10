0bee68a4d0d76ff75098600fb44785a482c34e4e
Johny Ho <johny@thoughtbot.com>
2023-12-09 19:09:28 -0500

    Expose method to all templates
    After the revert, we'd like to rename the method and make it accessible to all
    templates. The reasoning behind this is because superglue was using @virtual_path
    on the html templates, which seemed like a private variable, with this change we
    expose the active_template_virtual_path to everyone so we don't have to use private
    instance vars elsewhere.

--- lib/props_template/layout_patch.rb
+++ lib/props_template/layout_patch.rb
@@ -1,6 +1,12 @@
 module Props
   module LayoutPatch
     def render_template(view, template, layout_name, locals)
+      unless view.respond_to? :active_template_virtual_path
+        view.instance_eval <<~RUBY, __FILE__, __LINE__ + 1
+          def active_template_virtual_path; "#{template.virtual_path}";end
+        RUBY
+      end
+
       if template.respond_to?(:handler) && template.handler == Props::Handler && layout_name
         prepend_formats(template.format)
         render_props_template(view, template, layout_name, locals)
@@ -10,18 +16,10 @@ module Props
     end
 
     def render_props_template(view, template, path, locals)
-      view.instance_eval <<~RUBY, __FILE__, __LINE__ + 1
-        def virtual_path_of_template;"#{template.virtual_path}";end
-      RUBY
+      layout = resolve_layout(path, locals.keys, [formats.first])
 
-      # Deprecated: Usage of virtual_path_of_template in local_assigns will
-      # be removed for a method definition above
-      layout_locals = locals.dup
-      layout_locals[:virtual_path_of_template] = template.virtual_path
-
-      layout = resolve_layout(path, layout_locals.keys, [formats.first])
       body = if layout
-        layout.render(view, layout_locals) do |json|
+        layout.render(view, locals) do |json|
           template.render(view, locals)
         end
       else
