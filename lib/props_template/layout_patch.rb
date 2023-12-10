919b0bc36718293c9e16da814529e4a71be84cc5
jho406 <johny@thoughtbot.com>
2023-12-09 19:16:32 -0500

    Active template virtual path (#13)
    * Revert "Remove virtual_path_of_template. (#12)"
    
    This reverts commit 55ace47646f2d3b4380180fcacbfe3a49ff37621.
    
    * Expose method to all templates
    
    After the revert, we'd like to rename the method and make it accessible to all
    templates. The reasoning behind this is because superglue was using @virtual_path
    on the html templates, which seemed like a private variable, with this change we
    expose the active_template_virtual_path to everyone so we don't have to use private
    instance vars elsewhere.

--- lib/props_template/layout_patch.rb
+++ lib/props_template/layout_patch.rb
@@ -1,6 +1,12 @@
 module Props
   module LayoutPatch
     def render_template(view, template, layout_name, locals)
+      unless view.respond_to? :active_template_virtual_path
+        view.instance_eval <<~RUBY, __FILE__, __LINE__ + 1
+          def active_template_virtual_path; "#{template.virtual_path}";end
+        RUBY
+      end
+
       if template.respond_to?(:handler) && template.handler == Props::Handler && layout_name
         prepend_formats(template.format)
         render_props_template(view, template, layout_name, locals)
@@ -11,6 +17,7 @@ module Props
 
     def render_props_template(view, template, path, locals)
       layout = resolve_layout(path, locals.keys, [formats.first])
+
       body = if layout
         layout.render(view, locals) do |json|
           template.render(view, locals)
